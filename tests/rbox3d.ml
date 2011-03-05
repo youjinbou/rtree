f8b45dc81c931be4fb0740608ff05bb89103e4dd
Didier Cassirame <didier.cassirame@gmail.com>
2011-03-05 11:50:35 +0100

    * replaced the rbox class definition from the library by a module

---
+++ tests/rbox3d.ml
@@ -0,0 +1,129 @@
+open Rtree
+(* module implementing Region.T *)
+module Make =
+  functor (Coord : Vec.T) ->
+struct
+
+  class c (p1 : Coord.t) (p2 : Coord.t) = 
+    let b_ = Coord.map2 min p1 p2
+    and t_ = Coord.map2 max p1 p2
+    in
+  object(self)
+
+    method bottom = b_
+    method top    = t_
+
+    method private dims   = Coord.sub t_ b_
+
+(*
+    method center = 
+      let half = Coord.Scalar.div Coord.Scalar.one (Coord.Scalar.add Coord.Scalar.one Coord.Scalar.one)
+      in
+      let d = (Coord.scale self#dims half)
+      in 
+	Coord.add b_ d
+*)	
+  
+    (* volume covered by self *)
+    val area_ = 
+      let x = Coord.sub t_ b_ in
+	Coord.dot x x
+
+    (* whether the rbox rb overlaps with self *)
+    method overlaps (rb : c) = 
+      let segment_overlap s1 s2 e1 e2 =
+	(s1 < e2) && (s2 < e1)
+      in
+      let b1 = b_ 
+      and b2 = rb#bottom
+      and t1 = t_ 
+      and t2 = rb#top
+      in
+      let rec check_seg i =
+	if i = -1 
+	then true 
+	else
+	  let s1 = Coord.get b1 i
+	  and s2 = Coord.get b2 i
+	  and e1 = Coord.get t1 i
+	  and e2 = Coord.get t2 i
+	  in
+	    (segment_overlap s1 s2 e1 e2) && check_seg (pred i)
+      in
+	check_seg (pred Coord.size)
+
+    (* whether the rbox rb fits in self *)
+    method includes (rb : c) =
+      let segment_include s1 s2 e1 e2 =
+	(s1 <= s2) && (e2 <= e1)
+      in
+      let b1 = b_ 
+      and b2 = rb#bottom
+      and t1 = t_ 
+      and t2 = rb#top
+      in
+      let rec check_seg i =
+	if i = -1 
+	then true 
+	else
+	  let s1 = Coord.get b1 i
+	  and s2 = Coord.get b2 i
+	  and e1 = Coord.get t1 i
+	  and e2 = Coord.get t2 i
+	  in
+	    (segment_include s1 s2 e1 e2) && check_seg (pred i)
+      in
+	check_seg (pred Coord.size)
+	  
+    method area = area_
+
+    (*
+    (* whether self surrounds the point p *)
+    method surrounds p =
+      let rec check f p1 p2 i =
+	let c k = (f (Coord.get p1 k) (Coord.get p2 k)) 
+	in
+	  if i = 0
+	  then c 0
+	  else
+	    (c i) && (check f p1 p2 (pred i))
+      in
+	(check (<) p t_ (pred Coord.size)) && (check (>) p b_ (pred Coord.size))
+      *)
+
+    (* create new rbox covering self and the rbox rb *)
+    method expand (rb : c) =
+      let bottom = Coord.map2 min rb#bottom b_
+      and top    = Coord.map2 max rb#top t_
+      in
+	new c bottom top
+	  
+    method area_with (rb : c) = 
+      (self#expand rb)#area 
+
+    method to_string = string_of_int (Oo.id self)
+      
+  end
+
+  type t = c
+  type key_t = Coord.t
+  type scalar_t = Coord.Scalar.t
+
+(*
+  let expand (v : t) (r : t) : t = v#expand r
+  let area (v : t) : scalar_t = v#area
+  let area_with (v : t) (r : t) : scalar_t = v#area_with r
+  let overlaps (v : t) (r : t) : bool = v#overlaps r
+  let includes (v : t) (r : t) : bool = v#includes r
+  let to_string (v : t) : string = v#to_string
+*)
+
+  let expand v    = v#expand
+  let area v      = v#area
+  let area_with v = v#area_with
+  let overlaps v  = v#overlaps
+  let includes v  = v#includes
+  let to_string v = v#to_string
+
+
+end
---
+++ tests/tvec.ml
@@ -0,0 +1,64 @@
+
+module type SCALAR =
+sig
+  type t
+  val zero : t
+  val one : t
+  val add : t -> t -> t
+  val sub : t -> t -> t
+  val mul : t -> t -> t
+  val div : t -> t -> t
+  val opp : t -> t
+  val rand : t -> t
+  val modulo : t -> t -> t
+  val epsilon : t
+  val abs : t -> t
+  val sqrt : t -> t
+  val to_string : t -> string
+end
+
+module type T =
+sig
+  module Scalar : SCALAR
+  type t
+  type tuple_t
+  val size : int
+  val null : unit -> t
+  val one : unit -> t
+  val unit : int -> t
+  val of_tuple : tuple_t -> t
+  val to_tuple : t -> tuple_t
+  val get : t -> int -> Scalar.t
+  val set : t -> int -> Scalar.t -> unit
+  val map : (Scalar.t -> Scalar.t) -> t -> t
+  val map2 :
+    (Scalar.t -> Scalar.t -> Scalar.t) -> t -> t -> t
+  val map3 :
+    (Scalar.t -> Scalar.t -> Scalar.t -> Scalar.t) ->
+    t -> t -> t -> t
+  val map4 :
+    (Scalar.t ->
+       Scalar.t -> Scalar.t -> Scalar.t -> Scalar.t) ->
+    t -> t -> t -> t -> t
+  val opp : t -> t
+  val add : t -> t -> t
+  val add3 : t -> t -> t -> t
+  val add4 : t -> t -> t -> t -> t
+  val sub : t -> t -> t
+  val sub3 : t -> t -> t -> t
+  val sub4 : t -> t -> t -> t -> t
+  val scale : t -> Scalar.t -> t
+  val muladd : t -> Scalar.t -> t -> t
+  val dot : t -> t -> Scalar.t
+  val cross : t -> t -> t
+  val copy : t -> t -> unit
+  val random : t -> t
+  val modulo : t -> Scalar.t -> t
+  val below_epsilon : t -> bool
+  val for_all : (Scalar.t -> bool) -> t -> bool
+  val fold_left :
+    ('a -> Scalar.t -> 'a) -> 'a -> t -> 'a
+  val fold_right :
+    (Scalar.t -> 'a -> 'a) -> t -> 'a -> 'a
+  val to_string : t -> string
+end
